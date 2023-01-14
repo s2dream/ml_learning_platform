cf5ffbd8e32b54a3d0f7e20ec4cce17b7ba194bf
Jeonghoon Park <jeonghoon@Jeonghoonui-MacBookPro.local>
2023-01-14 11:39:27 +0900

    directory hierarchy change

---
+++ model/generative/vae/vae_models.py
@@ -0,0 +1,15 @@
+import copy
+from typing import Optional, Any, Union, Callable
+
+import torch
+from torch.nn.modules.transformer import Transformer
+from torch import Tensor
+from torch.nn import functional as F
+from torch.nn.modules import Module
+
+class VAEBasic(Module):
+    def __init__(self):
+        pass
+
+    def forward(self) -> Tensor:
+        pass
